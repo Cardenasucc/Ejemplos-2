869012f26e4005615dea00e4e940837d9b454751
Adriana <Adriana@LAPTOP-P9HSBAIV>
2022-11-13 00:11:49 -0500

    sus

--- Ej 2.11/Program.cs
+++ Ej 2.11/Program.cs
@@ -4,14 +4,23 @@ namespace ejemplo2_11
 {
     public class ejemplo2_11
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            int perimetro, preciomc;
-
-            Console.WriteLine("Digite el perimetro de la piscina:");
-            perimetro = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite el precio de la piscina por metro cubico");
-            preciomc = int.Parse(Console.ReadLine());
+            double alto, ancho, largo, pago, precio_por_m3;
+            double volumen_en_m3;
+            Console.Write("Ingresa el valor de alto en m: ");
+            alto = double.Parse(Console.ReadLine());
+            Console.Write("Ingresa el valor de ancho en m: ");
+            ancho = double.Parse(Console.ReadLine());
+            Console.Write("Ingresa el valor de largo en m: ");
+            largo = double.Parse(Console.ReadLine());
+            Console.Write("Ingresa el valor de precio por m3: ");
+            precio_por_m3 = double.Parse(Console.ReadLine());
+            volumen_en_m3 = alto * largo * ancho;
+            pago = volumen_en_m3 * precio_por_m3;
+            Console.WriteLine("Valor del pago: " + pago);
+            Console.WriteLine("Valor del volumen en m3: " + volumen_en_m3);
+            Console.WriteLine();
         }
     }
 }
